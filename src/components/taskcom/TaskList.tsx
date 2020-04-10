571ec15a7dfc12828dac63e439b068e2956248c1
sujashree <s.sujashree@gmail.com>
2020-04-10 12:48:44 +0530

    remove Task component and directly return list

--- src/components/taskcom/TaskList.tsx
+++ src/components/taskcom/TaskList.tsx
@@ -5,14 +5,31 @@ import Task from './Task';
 
 const TaskList = () => {
 
-    const { tasks } = useContext(TaskListContext);
+    const { tasks, removeTask, findItem } = useContext(TaskListContext);
     return (
 
         <div>
             <ul className="list">
                 {tasks.map((task: any) =>
                 {
-                    return <Task task={task} key={task.id} />
+                    //return <Task task={task} key={task.id} />
+                    return(
+                    <li className="list-item">
+                        <span>{task.title} </span>
+
+                        
+                            <button
+                                className="btn-delete task-btn"
+                                onClick={() => removeTask(task.id)}
+                            >
+                                <i className="fas fa-trash-alt"></i>
+                            </button>{' '}
+                            <button className="btn-edit task-btn" onClick={() => findItem(task.id)}>
+                                <i className="fas fa-pen"></i>
+                            </button>
+                        
+                        </li>
+                        )
                 }
                 )}
             </ul>
